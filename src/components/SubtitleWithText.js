0dfb12efc8358bb7ebd191c4b034172579d25692
Gezart <gezart.dervishi@gmail.com>
2022-11-04 21:20:56 +0100

    build after errors

--- src/components/SubtitleWithText.js
+++ src/components/SubtitleWithText.js
@@ -4,7 +4,7 @@ const SubtitleWithText = ({subtitleWithText}) => {
   return (
     <div className='subtitle-with-text'>
         <h3> {subtitleWithText?.theSubtitle}</h3>
-        <p dangerouslySetInnerHTML={{__html : subtitleWithText?.theContent }}></p>
+        <div dangerouslySetInnerHTML = {{ __html : subtitleWithText?.theContent }}></div>
     </div>
   )
 }
