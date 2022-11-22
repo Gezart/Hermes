2b45f0284985298818e00b5bee984492fe8cfb43
Gezart <gezart.dervishi@gmail.com>
2022-11-22 13:00:29 +0100

    add bottom section on homepage

--- src/components/TextWrapper.js
+++ src/components/TextWrapper.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const TextWrapper = ({textWrapper}) => {
+    console.log(textWrapper);
   return (
     <div className='subtitle-with-text'>
-      <h3>{textWrapper.theSubtitle}</h3>
-      <div dangerouslySetInnerHTML = {{ __html : textWrapper.theContent }}> </div>
+      <h3>{textWrapper?.theSubtitle}</h3>
+      <div dangerouslySetInnerHTML = {{ __html : textWrapper?.theContent }}> </div>
     </div>
   )
 }
